c7e25e2d63464e2118d0d49107aca278f8d65acb
elrorens <elrorens@gmail.com>
2023-07-30 15:40:33 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
 input.onButtonPressed(Button.A, function () {
-    servos.P2.setAngle(90)
+    servos.P1.setAngle(90)
 })
 input.onButtonPressed(Button.B, function () {
-    servos.P2.setAngle(180)
+    servos.P1.setAngle(180)
 })
-pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
 basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P1) == 0) {
+    if (pins.digitalReadPin(DigitalPin.P2) == 0) {
         music.play(music.tonePlayable(440, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
-        servos.P2.setAngle(135)
+        servos.P1.setAngle(135)
     } else {
-        servos.P2.setAngle(45)
+        servos.P1.setAngle(45)
     }
-    basic.showNumber(pins.digitalReadPin(DigitalPin.P1))
+    basic.showNumber(pins.digitalReadPin(DigitalPin.P2))
 })
